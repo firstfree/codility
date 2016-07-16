d53f337187fa6868dee46e4c15e8646dc1eea40d
JesusOh <ojsboy@hotmail.com>
2016-07-16 23:44:14 +0900

    lessons

---
+++ BinaryGap/BinartGap.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Solution(N int) int {
+	left, len := -1, 0
+
+	for N > 0 {
+		k := N & -N
+		right := int(math.Log2(float64(k)))
+
+		if (left != -1) && ((right - left - 1) > len) {
+			len = right - left - 1
+		}
+
+		left = right
+
+		N = N & (N - 1)
+	}
+
+	return len
+}
+
+func main() {
+	fmt.Println(Solution(1041))
+}
